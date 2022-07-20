e7fa9a674111999bacd935d672add06d0d9dabb4
O'neal Maoukola <maoukola.oneal@gmail.com>
2022-07-20 17:31:28 +0200

    initiating i18n but not tested yet

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  get 'opening_hours/new'
-  root to: 'shops#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :shops do
-    resources :opening_hours, only: [:new]
+
+  scope '(:locale)', locale: /fr|es/ do
+    get 'opening_hours/new'
+    root to: 'shops#index'
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    resources :shops do
+      resources :opening_hours, only: [:new]
+    end
   end
 end
